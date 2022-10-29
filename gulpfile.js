df9b518f918e74550598b71800a805261dd98e53
Beygs <boris.gilles@hotmail.fr>
2022-10-29 23:48:11 +0200

    🧹 chore: add gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+
+import gulp from "gulp";
+const { dest, src } = gulp;
+import ts from "gulp-typescript";
+import uglify from "gulp-uglify";
+
+const tsProject = ts.createProject("./tsconfig.json");
+
+const compileTs = () => {
+  return src("./src/**/*.ts")
+    .pipe(tsProject())
+    .pipe(uglify())
+    .pipe(dest("./build"))
+}
+
+const defaultTask = compileTs;
+
+export default defaultTask;
